d863a0a13697d24d1aa4fdbdd00cbe28c57ed7b7
Jorge Luis Martinez Gomez <jol@jol.dev>
2020-05-13 02:28:55 -0700

    make outline and first combinator

--- lib/parsby.rb
+++ lib/parsby.rb
@@ -3,4 +3,26 @@ require "parsby/version"
 module Parsby
   class Error < StandardError; end
   # Your code goes here...
+
+  class Combinator
+    def initialize(&b)
+      @parser = b
+    end
+
+    def parse(io)
+      @parser.call io
+    end
+  end
+
+  def self.string(e)
+    Combinator.new do |io|
+      a = io.read e.length
+      if a == e
+        a
+      else
+        a.chars.each {|ac| io.ungetc ac }
+        raise Error
+      end
+    end
+  end
 end
