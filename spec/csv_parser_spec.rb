4420fab8c631ab26169360a89d7be16f33bc9c0e
Jorge Luis Martinez Gomez <jol@jol.dev>
2020-05-14 05:46:54 -0700

    add CsvParser test

---
+++ spec/csv_parser_spec.rb
@@ -0,0 +1,22 @@
+RSpec.describe CsvParser do
+  let :simple_csv do
+    <<~EOF
+      foo,bar
+      1,2
+      3,4
+    EOF
+  end
+
+  it "parsers simple csv files" do
+    expect(CsvParser.parse <<~CSV)
+      foo,bar
+      1,2
+      3,4
+    CSV
+      .to eq [
+        ["foo", "bar"],
+        ["1", "2"],
+        ["3", "4"],
+      ]
+  end
+end
