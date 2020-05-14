c5bbfe85b24173d5060ddfc43c1f745fdc152eab
Jorge Luis Martinez Gomez <jol@jol.dev>
2020-05-14 05:16:28 -0700

    test Parsy.many

--- spec/parsby_spec.rb
+++ spec/parsby_spec.rb
@@ -3,6 +3,18 @@ RSpec.describe Parsby do
     expect(Parsby::VERSION).not_to be nil
   end
 
+  describe :many do
+    it "applies parser repeatedly and returns a list of the results" do
+      expect(Parsby.many(Parsby.string("foo")).parse "foofoofoo")
+        .to eq ["foo", "foo", "foo"]
+    end
+
+    it "returns empty list when parser couldn't be applied" do
+      expect(Parsby.many(Parsby.string("bar")).parse "foofoofoo")
+        .to eq []
+    end
+  end
+
   describe Parsby::BackedIO do
     let(:pipe) { IO.pipe }
     let(:r) { pipe[0] }
