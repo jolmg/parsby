701ed20af172ea57972dd9464a8f9da58f86b525
Jorge Luis Martinez Gomez <jol@jol.dev>
2020-05-29 00:48:20 -0700

    include module to avoid having to specify Parsby all the time

--- spec/parsby/combinators_spec.rb
+++ spec/parsby/combinators_spec.rb
@@ -1,45 +1,47 @@
 RSpec.describe Parsby::Combinators do
+  include Parsby::Combinators
+
   describe :many do
     it "applies parser repeatedly and returns a list of the results" do
-      expect(Parsby.many(Parsby.string("foo")).parse "foofoofoo")
+      expect(many(string("foo")).parse "foofoofoo")
         .to eq ["foo", "foo", "foo"]
     end
 
     it "returns empty list when parser couldn't be applied" do
-      expect(Parsby.many(Parsby.string("bar")).parse "foofoofoo")
+      expect(many(string("bar")).parse "foofoofoo")
         .to eq []
     end
   end
 
   describe :any_char do
     it "raises exception on EOF" do
-      expect { Parsby.any_char.parse "" }
+      expect { any_char.parse "" }
         .to raise_error Parsby::ExpectationFailed
     end
   end
 
   describe :decimal do
     it "parses decimal numbers" do
-      expect(Parsby.decimal.parse("123")).to eq 123
+      expect(decimal.parse("123")).to eq 123
     end
 
     it "does not accept anything other than sign-less positive decimal integers" do
-      expect(Parsby.decimal.parse("123.45")).to eq 123
-      expect { Parsby.decimal.parse("-123") }
+      expect(decimal.parse("123.45")).to eq 123
+      expect { decimal.parse("-123") }
         .to raise_error Parsby::ExpectationFailed
-      expect { Parsby.decimal.parse("+123") }
+      expect { decimal.parse("+123") }
         .to raise_error Parsby::ExpectationFailed
     end
 
     it "expects at least one decimal digit" do
-      expect { Parsby.decimal.parse("foo") }
+      expect { decimal.parse("foo") }
         .to raise_error Parsby::ExpectationFailed
     end
   end
 
   describe :string do
     it "parses the string provided" do
-      expect(Parsby.string("foo").parse("foo")).to eq "foo"
+      expect(string("foo").parse("foo")).to eq "foo"
     end
     
     # XXX: Backtracking should be handled BackedIO, but I've implemented
@@ -49,11 +51,11 @@ RSpec.describe Parsby::Combinators do
     # own or not, and I'd like to know why.
     it "backtracks properly on failure" do
       s = StringIO.new "barbaz"
-      expect { Parsby.string("foo").parse(s) }.to raise_error Parsby::ExpectationFailed
+      expect { string("foo").parse(s) }.to raise_error Parsby::ExpectationFailed
       expect(s.read).to eq "barbaz"
 
       s = StringIO.new "baz"
-      expect { Parsby.string("foobar").parse(s) }.to raise_error Parsby::ExpectationFailed
+      expect { string("foobar").parse(s) }.to raise_error Parsby::ExpectationFailed
       expect(s.read).to eq "baz"
     end
   end
