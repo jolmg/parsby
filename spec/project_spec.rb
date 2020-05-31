e7472bca2e6eef5d7a382009ef7b32fdd0880c3d
Jorge Luis Martinez Gomez <jol@jol.dev>
2020-05-31 01:56:26 -0700

    add test to check for testing coverage

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -2,4 +2,8 @@ RSpec.describe "the project" do
   it "has no method without documentation" do
     expect(`./bin/methods-with-pending-documentation`).to eq ""
   end
+
+  it "has no untested method" do
+    expect(`bash -c 'comm -23 <(./bin/all-methods) <(./bin/tested-methods)'`).to eq ""
+  end
 end
