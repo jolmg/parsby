b3c78daa31e10e7279cdb7a0b2e20cb4ec675e54
Jorge Luis Martinez Gomez <jol@jol.dev>
2020-07-09 20:34:37 -0700

    test for vestigial methods

--- spec/00_project_spec.rb
+++ spec/00_project_spec.rb
@@ -6,4 +6,8 @@ RSpec.describe "the project" do
   it "has no untested method" do
     expect(`bash -c 'comm -23 <(./bin/all-methods) <(./bin/tested-methods)'`).to eq ""
   end
+
+  it "has no vestigial method" do
+    expect(`./bin/vestigial-methods`).to eq ""
+  end
 end
