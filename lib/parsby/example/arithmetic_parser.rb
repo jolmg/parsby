0ee9f2ff207f283ede25b02cfd4301089ae617da
Jorge Luis Martinez Gomez <jol@jol.dev>
2020-09-12 18:29:33 -0700

    add arithmetic parser

---
+++ lib/parsby/example/arithmetic_parser.rb
@@ -0,0 +1,96 @@
+require "parsby"
+
+module Parsby::Example
+  module ArithmeticParser
+    include Parsby::Combinators
+    extend self
+
+    def parse(io)
+      expr.parse io
+    end
+
+    def self.define_binary_op(name, op)
+      define_combinator name do |left, right|
+        group(left, spaced(ilit(op)), right)
+      end
+    end
+
+    define_binary_op :add_op, "+"
+    define_binary_op :sub_op, "-"
+    define_binary_op :mul_op, "*"
+    define_binary_op :div_op, "/"
+    define_binary_op :exp_op, "^"
+
+    def self.define_unary_op(name, op)
+      define_combinator name do |subexpr|
+        group(ilit(op), ws > subexpr)
+      end
+    end
+
+    define_unary_op :neg_op, "-"
+    define_unary_op :pos_op, "+"
+
+    # hpe - higher precedence level
+    # spe - same precedence level
+
+    def right_associative_binary_precedence_level(hpe, operators)
+      recursive do |spe|
+        choice(
+          *operators.map do |op|
+            send(op, hpe, spe)
+          end,
+          hpe,
+        )
+      end
+    end
+
+    def left_associative_binary_precedence_level(hpe, operators)
+      reduce hpe do |left_expr|
+        choice(
+          *operators.map do |op|
+            send(op, pure(left_expr), hpe)
+          end
+        )
+      end
+    end
+
+    def unary_precedence_level(hpe, operators)
+      recursive do |spe|
+        choice(
+          *operators.map do |op|
+            send(op, spe)
+          end,
+          hpe,
+        )
+      end
+    end
+
+    define_combinator :expr do
+      lazy do
+        e = choice(
+          decimal_fraction,
+          between(lit("("), lit(")"), expr),
+        )
+
+        e = right_associative_binary_precedence_level(e, [
+          :exp_op,
+        ])
+
+        e = unary_precedence_level(e, [
+          :neg_op,
+          :pos_op,
+        ])
+
+        e = left_associative_binary_precedence_level(e, [
+          :mul_op,
+          :div_op,
+        ])
+
+        e = left_associative_binary_precedence_level(e, [
+          :add_op,
+          :sub_op,
+        ])
+      end
+    end
+  end
+end
